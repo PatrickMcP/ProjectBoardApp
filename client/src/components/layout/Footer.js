94225e1ea9ab162e639ae5f58a6a59e56d87acbd
Patrick <patrick.c.mcpartland@gmail.com>
2019-01-17 19:32:57 -0700

    Create Repo

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default () => {
   return (
-    <footer className="bg-dark text-white mt-5 p-4 text-center fixed-bottom">
+    <footer className="bg-dark text-white mt-5 p-4 text-center fixed-bottom mt-5 pt-5">
       Copyright &copy; {new Date().getFullYear()} BCProject 3 Thomas Dean Patrick McPartland
     </footer>
   );
