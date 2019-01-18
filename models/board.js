a4414e5bb6093ed08c9af27a419dd35a75e9f9eb
Patrick <patrick.c.mcpartland@gmail.com>
2019-01-17 19:42:32 -0700

    Bringing in the other models and controlers

---
+++ models/board.js
@@ -0,0 +1,21 @@
+/*
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// this will be our data base's data structure 
+const BoardSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    lists: [{
+      type: Schema.Types.ObjectId,
+      ref: "List"
+    }]
+  }
+);
+
+// export the new Schema so we could modify it using Node.js
+module.exports = mongoose.model("Board", BoardSchema);
+*/
---
+++ models/list.js
@@ -0,0 +1,21 @@
+/*
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// this will be our data base's data structure 
+const ListSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    cards: [{
+      type: Schema.Types.ObjectId,
+      ref: "Card"
+    }]
+  }
+);
+
+// export the new Schema so we could modify it using Node.js
+module.exports = mongoose.model("List", ListSchema);
+*/
