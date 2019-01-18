35526b4e4e88d187c1ff497f62736d3a77063b92
Patrick <patrick.c.mcpartland@gmail.com>
2019-01-17 20:54:04 -0700

    redux fix

--- client/src/store.js
+++ client/src/store.js
@@ -4,15 +4,21 @@ import rootReducer from './reducers';
 
 const initialState = {};
 
-const middleware = [thunk];
+const store = () => {
+  const middleware = [thunk];
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+  const store = createStore(
+    rootReducer,
+    compose(
+      applyMiddleware(...middleware),
+      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
+    )
+  );
+
+  return store;
+};
 
 export default store;
